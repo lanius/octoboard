9af98807390e2da3125bb3e946fca91a1812867a
lanius <lanius@nirvake.org>
2012-03-10 03:06:57 +0900

    refactor diff.

--- extension/js/diff.js
+++ extension/js/diff.js
@@ -7,71 +7,82 @@
   
   var hidePolicy = 'frame';
   
-  var setShowDiff = function (pushList) {
-    pushList.each(function (idx, issue) {
-      
-      var commits = $(issue).find(".commits ul");
-      commits.children().each(function (idx, co) {
-        var commit = $(co);
-        var link = $('<a>');
-        link.html(SHOW_LABEL);
-        link.addClass(LABEL_CLASS);
-        var url = commit.find('code a').attr('href');
-        if (url === undefined) {
-          return;
-        }
-        
-        link.on('click', function () {
-          var container = commit.find('.' + DIFF_CONTAINER_CLASS);
-          if (container.length !== 0) {
-            if (link.html() === HIDE_LABEL) {
-              container.hide();
-              link.html(SHOW_LABEL);
-            }
-            else {
-              container.show();
-              link.html(HIDE_LABEL);
-            }
-            return;
-          }
-          
-          var loading =  $('<span> loading...</span>');
-          commit.append(loading);
-          
-          $.get(url).then(function (data, status, xhr) {
-            var diff = $(data).find('#files');
-            
-            var frame = diff.find('.meta');
-            frame.addClass('gdbd-diff-frame');
-            
-            var container = $('<div>');
-            container.addClass(DIFF_CONTAINER_CLASS);
-            container.append(diff);
-            
-            if (hidePolicy === 'frame') {
-              frame.on('click', function () {
-                container.hide();
-                link.html(SHOW_LABEL);
-              });
-            }
-            else { // hidePolicy === 'any'
-              container.on('click', function () {
-                container.hide();
-                link.html(SHOW_LABEL);
-              });
-            }
-            
-            loading.remove();
-            commit.append(container);
-          });
-          
-          link.html(HIDE_LABEL);
-        });
-        commit.append(link);
-      });
+  var setShowDiff = function (pushes) {
+    pushes.each(function (idx, push) {
+      setShowDiffToPush($(push));
     });
   };
-
+  
+  var setShowDiffToPush = function (push) {
+    var commits = push.find(".commits ul");
+    commits.children().each(function (idx, commit) {
+      setShowDiffToCommit($(commit));
+    });
+  };
+  
+  var setShowDiffToCommit = function (commit) {
+    var link = $('<a>');
+    link.html(SHOW_LABEL);
+    link.addClass(LABEL_CLASS);
+    var url = commit.find('code a').attr('href');
+    if (!url) {
+      return; // the case of "n more commits"
+    }
+    link.on('click', function () {
+      toggleDiff(commit);
+    });
+    commit.append(link);
+  };
+  
+  var toggleDiff = function (commit) {
+    var container = commit.find('.' + DIFF_CONTAINER_CLASS);
+    var link = commit.find('.' + LABEL_CLASS);
+    var url = commit.find('code a').attr('href');
+    if (container.length !== 0) {
+      if (link.html() === HIDE_LABEL) {
+        container.hide();
+        link.html(SHOW_LABEL);
+      }
+      else {
+        container.show();
+        link.html(HIDE_LABEL);
+      }
+      return;
+    }
+    
+    var loading =  $('<span> loading...</span>');
+    commit.append(loading);
+    
+    $.get(url).then(function (data, status, xhr) {
+      var diff = $(data).find('#files');
+      
+      var frame = diff.find('.meta');
+      frame.addClass('gdbd-diff-frame');
+      
+      var container = $('<div>');
+      container.addClass(DIFF_CONTAINER_CLASS);
+      container.append(diff);
+      
+      if (hidePolicy === 'frame') {
+        frame.on('click', function () {
+          container.hide();
+          link.html(SHOW_LABEL);
+        });
+      }
+      else { // hidePolicy === 'any'
+        container.on('click', function () {
+          container.hide();
+          link.html(SHOW_LABEL);
+        });
+      }
+      
+      loading.remove();
+      commit.append(container);
+    });
+    
+    link.html(HIDE_LABEL);
+  };
+  
   var getAllPushes = function () {
     return $(".alert.push").children();
   };
@@ -80,7 +91,7 @@
     return $(".pagination.ajax_paginate a");
   };
 
-  var diff = function(large, small) {
+  var diff = function(large, small) { // this does not specify "diff" of git!
     return large.filter(function (i, obj) {
       return !(small.index(obj) > -1); 
     });
